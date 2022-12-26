6a049b83d06c20995c070018234b4258ac6028c5
Midokhalil1 <midokhalil22@gmail.com>
2022-12-26 14:06:16 -0500

    if statment

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 # while loops
-n = 0
-while n < 5:
-    print(n)
-    n += 1
+# n = 0
+# while n < 5:
+#     print(n)
+#     n += 1
 
+# test
+
+A = 20
+B = 20
+C = 11
+
+if A == C :
+    print('Yes')
+else :
+    print('No')
 
 
