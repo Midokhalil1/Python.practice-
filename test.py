a75787b4099025b2a85a9e7583070af147c60cda
Midokhalil1 <midokhalil22@gmail.com>
2022-12-26 14:12:10 -0500

    logic operator

--- test.py
+++ test.py
@@ -4,15 +4,24 @@
 #     print(n)
 #     n += 1
 
-# test
+# if statment
 
 A = 20
 B = 20
 C = 11
 
-if A == C :
-    print('Yes')
-else :
-    print('No')
+# if A == C :
+#     print('Yes')
+# else :
+#     print('No')
 
+#logic operator
+
+if A > 10 or C > 10 :
+    print("Yes")
+else : 
+    print("No")
+
+
+ 
 
