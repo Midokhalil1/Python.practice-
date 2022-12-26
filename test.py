88a799107f1be7455254b75d80cb09627cd9f396
Midokhalil1 <midokhalil22@gmail.com>
2022-12-26 15:09:26 -0500

    whileLoop

--- test.py
+++ test.py
@@ -6,9 +6,9 @@
 
 # if statment
 
-A = 20
-B = 20
-C = 11
+#A = 20
+#B = 20
+#C = 11
 
 # if A == C :
 #     print('Yes')
@@ -17,18 +17,18 @@ C = 11
 
 #logic operator
 
-if A > 10 or C > 10 :
-    print("Yes")
-else : 
-    print("No")
+#if A > 10 or C > 10 :
+   # print("Yes")
+#else : 
+    #print("No")
 
 
  #identity operator 
  
-if A is B :
-    print("Yes")
-else : 
-    print("No")
+#if A is B :
+    #print("Yes")
+#else : 
+    #print("No")
 
 
 
@@ -60,3 +60,22 @@ else :
 list_names= ["Mido", "Ryan", "Khalil", "Emily", "Amira", "Athar"]
 for x in list_names:
     print(x)
+    
+# Loops using Range
+for i in range(10):
+    print ("Hello World")
+
+#While loops   
+
+Magic_number = 0
+while Magic_number < 10 :
+    print("Its Magic!")
+    Magic_number = Magic_number + 1
+
+# list of numbers
+list1 = [10, 24, 4, 45, 66, 93]
+num = 0
+ 
+for num in list1:
+    if num % 2 == 0:
+        print(num, end=" ")
