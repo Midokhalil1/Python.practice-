5881ca19da8eec8edc90be8bef8481988040c663
Midokhalil1 <midokhalil22@gmail.com>
2022-12-27 13:23:47 -0500

    rainbow colored

--- hello.py
+++ hello.py
@@ -4,4 +4,13 @@ from termcolor import colored, cprint
 
 text = colored ('hello, world', 'white', attrs=['reverse', 'blink'])
 print(text)
-cprint('Hello, Mido!', 'green', 'on_green')
+cprint('Hello, Mido!😃', 'magenta', 'on_green')
+for i in range (1):
+    cprint (i, 'blue', end=' ')
+
+print(colored('😃RAINBOW😃', 'red')) 
+print(colored('😃RAINBOW😃', 'yellow'))
+print(colored('😃RAINBOW😃', 'green'))   
+print(colored('😃RAINBOW😃', 'cyan'))   
+print(colored('😃RAINBOW😃', 'blue')) 
+print(colored('😀RAINBOW😀', 'magenta')) 
